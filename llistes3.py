fdf23b9c94ee6aa5da06d94d913fbef341884a2c
BuildTools <a.feo@sapalomera.cat>
2021-03-26 15:22:26 +0100

    Fix llistes3

--- llistes3.py
+++ llistes3.py
@@ -3,23 +3,23 @@ ingressos = 0
 llDespeses = []
 llIngressos = []
 
-diners = float(input("Quants diners tens?"))
+diners = float(input("Quants diners tens? "))
 
 while True:
-    operacio = input("Vols (i)ngresar o (g)astar?")
+    operacio = input("Vols (i)ngresar o (g)astar? ")
     if operacio == "i":
-        ingres = float(input("Quants vols ingressar?"))
+        ingres = float(input("Quants vols ingressar? "))
         diners += ingres
         ingressos += ingres
         llIngressos.append(ingres)
-        llDespeses.append("             ")
+        llDespeses.append("-")
         print("Tens " + str(diners) + "€.")
         print("Has ingressat un total de " + str(ingressos) + "€.")
     elif operacio == "g":
-        cost = float(input("Quants has gastat?"))
+        cost = float(input("Quants has gastat? "))
         diners -= cost
         despeses += cost
-        llIngressos.append("            ")
+        llIngressos.append("-")
         llDespeses.append(cost)
         print("Tens " + str(diners) + "€.")
         print("Has gastat un total de " + str(despeses) + "€.")
@@ -30,5 +30,5 @@ while True:
         break
 
 print("Ingressos    Despesses")
-for ingres in llIngressos:
-    print(ingres, "", llDespeses[llIngressos.index(ingres)])
+for ingres, despeses in zip(llIngressos, llDespeses):
+    print(str(ingres).replace("-", "            "), str(despeses).replace("-", "            "))
