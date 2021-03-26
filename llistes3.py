2c34849d0103dc1a848c18395fe67f2951756e53
BuildTools <a.feo@sapalomera.cat>
2021-03-26 15:28:58 +0100

    Espacios

--- llistes3.py
+++ llistes3.py
@@ -7,24 +7,28 @@ diners = float(input("Quants diners tens? "))
 
 while True:
     operacio = input("Vols (i)ngresar o (g)astar? ")
+
     if operacio == "i":
         ingres = float(input("Quants vols ingressar? "))
         diners += ingres
         ingressos += ingres
         llIngressos.append(ingres)
         llDespeses.append("-")
-        print("Tens " + str(diners) + "€.")
         print("Has ingressat un total de " + str(ingressos) + "€.")
+        print("Tens " + str(diners) + "€.")
+
     elif operacio == "g":
         cost = float(input("Quants has gastat? "))
         diners -= cost
         despeses += cost
         llIngressos.append("-")
         llDespeses.append(cost)
-        print("Tens " + str(diners) + "€.")
         print("Has gastat un total de " + str(despeses) + "€.")
+        print("Tens " + str(diners) + "€.")
+        
     else:
         break
+
     if diners < 0:
         print("T'has gastat més dels que tens")
         break
